5cf5381bf8f8956961ae989d11bd02cc11b5cd6c
ismaeldcent04 <ismadadi.26@gmail.com>
2025-03-01 20:08:10 -0400

    starting redux section

---
+++ 20-react-redux/src/components/Counter.js
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={() => dispatch({ type: "increment" })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          Decrement
+        </button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
