00f3edf3fb83cb099b713283d4fc8679da06915a
ismaeldcent04 <ismadadi.26@gmail.com>
2025-03-07 17:57:47 -0400

    Starting react router section

---
+++ 22-react-router/src/App.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { Products } from "./pages/Products";
+import { RootLayout } from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ 22-react-router/src/components/MainNavigation.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export const MainNavigation = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/products"}>Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
