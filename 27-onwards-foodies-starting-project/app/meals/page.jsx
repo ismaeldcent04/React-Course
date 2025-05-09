88b95d6a854fd51cecccc089b009350847aaa75d
ismaeldcent04 <ismadadi.26@gmail.com>
2025-05-09 08:11:17 -0400

    varios

--- 27-onwards-foodies-starting-project/app/meals/page.jsx
+++ 27-onwards-foodies-starting-project/app/meals/page.jsx
@@ -1,7 +1,15 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
-export default function MealsPage() {
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +25,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
